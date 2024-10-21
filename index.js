fe8f4be644cddf627a8e93496fdc83f7b06d7a47
Quintin Stewart <108433446+Quintentional@users.noreply.github.com>
2024-10-21 19:19:14 -0400

    changed svg for color adaptability

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const button = document.querySelector('button');
 const share = document.querySelector('.share-div');
 const triangle = document.querySelector('.triangle');
+const svg = document.querySelector(svg);
 
 button.addEventListener('click', function() {
     share.classList.toggle('active');
     triangle.classList.toggle('block');
+    // button.classList.toggle('clicked');
+    // svg.classList.toggle('fill');
 })
